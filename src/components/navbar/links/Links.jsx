6e60f2692ddceb2db7eb4457f20b1688e37f0159
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2024-01-22 13:43:53 +0700

    feat: adding feature authenticate login register

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -4,31 +4,31 @@ import Image from "next/image";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
 import { useState } from "react";
+import { handleLogout } from "@/lib/actions";
+import { auth } from "@/lib/auth";
 const { default: Link } = require("next/link");
 
-const Links = () => {
-  const links = [
-    {
-      title: "Homepage",
-      path: "/",
-    },
-    {
-      title: "About",
-      path: "/about",
-    },
-    {
-      title: "Contact",
-      path: "/contact",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    },
-  ];
-
+const links = [
+  {
+    title: "Homepage",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
+const Links = ({ session }) => {
   const [open, setOpen] = useState(false);
 
-  const session = true;
   const isAdmin = true;
 
   return (
@@ -37,10 +37,14 @@ const Links = () => {
         {links.map((link) => (
           <NavLink item={link} key={link.title} />
         ))}
-        {session ? (
+        {session?.user ? (
           <>
-            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
-            <button className={styles.logout}>Logout</button>
+            {session.user?.isAdmin && (
+              <NavLink item={{ title: "Admin", path: "/admin" }} />
+            )}
+            <form action={handleLogout}>
+              <button className={styles.logout}>Logout</button>
+            </form>
           </>
         ) : (
           <NavLink item={{ title: "Login", path: "/login" }} />
--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,7 +1,9 @@
 "use server";
 import { connectToDb } from "./utils";
-import { Post } from "./models";
+import { Post, User } from "./models";
 import { revalidatePath } from "next/cache";
+import { signIn, signOut } from "./auth";
+import bcrypt from "bcryptjs";
 
 export const addPost = async (formData) => {
   const { title, desc, slug, userId } = Object.fromEntries(formData);
@@ -33,6 +35,66 @@ export const deletePost = async (formData) => {
     revalidatePath("/blog");
   } catch (error) {
     console.log(error);
+    return { error: "something went wrong!" };
+  }
+};
+
+export const handleGithubLogin = async () => {
+  "use server";
+  await signIn("github");
+};
+
+export const handleLogout = async () => {
+  "use server";
+  await signOut();
+};
+
+export const register = async (previousState, formData) => {
+  const { username, email, img, password, passwordRepeat } =
+    Object.fromEntries(formData);
+
+  if (password !== passwordRepeat) {
+    return { error: "Password do not match" };
+  }
+
+  try {
+    connectToDb();
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return { error: "username already exists" };
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      img,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    console.log("saved to db");
+    return { success: true };
+  } catch (error) {
+    console.log(error);
+    return { error: "something went wrong!" };
+  }
+};
+
+export const login = async (previousState, formData) => {
+  const { username, password } = Object.fromEntries(formData);
+
+  try {
+    await signIn("credentials", { username, password });
+  } catch (error) {
+    console.log(error);
+    if (error.message.includes("CredentialsSignin")) {
+      return { error: "Invalid username or password!" };
+    }
     return { error: "something went wrong!" };
   }
 };
